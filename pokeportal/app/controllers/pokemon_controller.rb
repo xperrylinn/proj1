775f602e4c6459969c28368e852ca24cd63c59c8
Xavier Linn <xperrylinn@berkeley.edu>
2018-10-30 16:24:14 -0700

    fin

--- pokeportal/app/controllers/pokemon_controller.rb
+++ pokeportal/app/controllers/pokemon_controller.rb
@@ -12,7 +12,26 @@ class PokemonController < ApplicationController
 		else
 			Pokemon.find(params[:id]).update(Health: pokeHealth)
 		end
-		redirect_to "/trainers/#{current_trainer.id}"
+		#redirect_to "/trainers/#{current_trainer.id}"
+		redirect_to trainer_path(current_trainer) 
  	end
+
+        def new
+		render 'new/new'
+        end
+	
+	def create
+		@pokemon = Pokemon.new
+		@pokemon.name = params[:pokemon][:name] 
+		@pokemon.level = 1
+		@pokemon.Health = 100
+		@pokemon.trainer = current_trainer 
+		if @pokemon.save
+			redirect_to trainer_path(id: current_trainer)
+		else 
+			flash[:error] = @pokemon.errors.full_messages.to_sentence
+			redirect_to new_path
+		end
+	end
 end
 
--- pokeportal/config/routes.rb
+++ pokeportal/config/routes.rb
@@ -5,4 +5,7 @@ Rails.application.routes.draw do
   	patch 'capture/:id', to: 'pokemon#capture', as: 'capture'
   	patch 'damage/:id', to: 'pokemon#damage', as: 'damage'
 	get 'damage/:id', to: 'trainers#show'
+	get 'new', to: 'pokemon#new', as: 'new'
+	post 'create', to: 'pokemon#create', as: 'create'
+	post '/trainers/:id', to: 'pokemons#new'
 end
